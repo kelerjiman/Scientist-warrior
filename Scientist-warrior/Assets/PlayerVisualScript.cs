8ddc09be6161841a041ab412e586495098f70b2d
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-07-24 18:26:03 +0430

    Create PlayerVisualScript.cs

---
+++ Scientist-warrior/Assets/PlayerVisualScript.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+[Serializable]
+public class PlayerBodyPart
+{
+    public string Name;
+    [SerializeField] Sprite m_Sprite;
+
+    public Sprite Sprite
+    {
+        get { return m_Sprite; }
+        set
+        {
+            m_Sprite = value;
+            if (value != null)
+            {
+                PartHodler.sprite = value;
+            }
+        }
+    }
+
+    public SpriteRenderer PartHodler;
+
+}
+public class PlayerVisualScript : MonoBehaviour
+{
+    public List<PlayerBodyPart> playerBodyParts;
+    public static PlayerVisualScript Instance;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void SetItemVisual(string name,Sprite sprite)
+    {
+        foreach (var bodyPart in playerBodyParts)
+        {
+            if (bodyPart.Name == name)
+            {
+                bodyPart.Sprite = sprite;
+                return;
+            }
+        }
+    }
+
+    [CanBeNull]
+    public Sprite GetItemVisual(string name)
+    {
+        foreach (var bodyPart in playerBodyParts)
+        {
+            if (bodyPart.Name == name)
+                return bodyPart.Sprite;
+        }
+
+        return null;
+    }
+}
