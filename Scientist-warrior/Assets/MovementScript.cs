db130b4d1dfe846d3967ab0153d79c1315868699
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-07-24 16:11:05 +0430

    Update MovementScript.cs

--- Scientist-warrior/Assets/MovementScript.cs
+++ Scientist-warrior/Assets/MovementScript.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class MovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 m_Direction = Vector2.zero;
+    public int speed = 5;
+    public Rigidbody2D rigidbody2d;
+    private Vector2 m_TempPos;
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_Direction.x = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        m_Direction.y = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        if (m_Direction.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        if (m_Direction.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        m_TempPos = transform.position;
+        m_TempPos.x += m_Direction.x;
+        m_TempPos.y += m_Direction.y;
+
+
+        rigidbody2d.MovePosition(rigidbody2d.position + m_Direction * speed * Time.fixedDeltaTime);
     }
 }
