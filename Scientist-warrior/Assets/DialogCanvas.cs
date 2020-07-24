c07efc1b8185261a19ffcac256ace4c015128a8d
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-07-24 18:25:31 +0430

    Create DialogCanvas.cs

---
+++ Scientist-warrior/Assets/DialogCanvas.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogCanvas : MonoBehaviour
+{
+    public static DialogCanvas Instance;
+    [SerializeField] private CanvasRenderer dialogPanel;
+    [SerializeField] private Text dialogTextBox;
+    [SerializeField] private Image dialogPanelCharacterImage,PlayerImage;
+    [SerializeField] Sprite defaultImage;
+
+    public void PrintMessage(string message,[CanBeNull] Sprite sprite, [CanBeNull] Sprite PlayerSprite)
+    {
+        if (sprite != null)
+        {
+            dialogPanelCharacterImage.sprite = sprite;
+        }
+
+        if (PlayerSprite != null)
+        {
+            PlayerImage.sprite = PlayerSprite;
+        }
+        dialogPanel.gameObject.SetActive(true);
+        if (message == string.Empty)
+            dialogTextBox.text = string.Empty;
+        dialogTextBox.text = message;
+    }
+
+    public void ResetDialogPanel()
+    {
+        dialogTextBox.text = string.Empty;
+        dialogPanelCharacterImage.sprite = defaultImage;
+        dialogPanel.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        Instance = this;
+    }
+}
