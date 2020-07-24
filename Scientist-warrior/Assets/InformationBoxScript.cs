38809e142fe0b1cc127141ba1b2dd3adced2dd9c
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-07-24 18:25:43 +0430

    Create InformationBoxScript.cs

---
+++ Scientist-warrior/Assets/InformationBoxScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformationBoxScript : MonoBehaviour
+{
+    [SerializeField] private string message;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DialogCanvas.Instance.PrintMessage(message,null,PlayerVisualScript.Instance.GetItemVisual("Refrence"));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DialogCanvas.Instance.ResetDialogPanel();
+            Destroy(gameObject);
+        }
+    }
+}
