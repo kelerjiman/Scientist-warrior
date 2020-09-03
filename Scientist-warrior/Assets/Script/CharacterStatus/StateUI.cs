89d4dbd6c42c70a27b6c5f80fc672a98423568d6
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-03 22:52:34 +0430

    Update StateUI.cs

--- Scientist-warrior/Assets/Script/CharacterStatus/StateUI.cs
+++ Scientist-warrior/Assets/Script/CharacterStatus/StateUI.cs
@@ -1,53 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Script.CharacterStatus
 {
     public class StateUI : MonoBehaviour, IStateUI
     {
-        [SerializeField] private CharacterState characterState;
+        [SerializeField] private List<State> statesList=new List<State>();
         [SerializeField] private RectTransform stateSlotParent;
         [SerializeField] private StateSlot stateSlot;
         public List<StateSlot> m_StateSlots;
-
-        private void OnValidate()
+        private void Start()
         {
-            if (stateSlotParent != null && stateSlotParent.childCount == 0)
-                for (int i = 0; i < characterState.states.Count; i++)
-                {
-                    m_StateSlots.Add(Instantiate(stateSlot, stateSlotParent));
-                    m_StateSlots[i].state = characterState.states[i];
-                }
-        }
-
-        private void Awake()
-        {
-            for (int i = 0; i < characterState.states.Count; i++)
+            statesList = StateManager.Instance.characterState.GetCopy();
+            foreach (var state in statesList)
             {
-                m_StateSlots.Add(Instantiate(stateSlot, stateSlotParent));
-                m_StateSlots[i].state = characterState.states[i];
+                var x=Instantiate(stateSlot, stateSlotParent);
+                x.State = state;
+                m_StateSlots.Add(x);
             }
         }
 
+
         public void RemoveState(State state)
         {
-            if (m_StateSlots.Find(slot => slot.state.type == state.type).State != null)
+            foreach (var slot in m_StateSlots)
             {
-                State temp = m_StateSlots.Find(slot => slot.state.type == state.type).State;
-                temp.amount = m_StateSlots.Find(slot => slot.state.type == state.type).State.amount - state.amount;
-                m_StateSlots.Find(slot => slot.state.type == state.type).State = temp;
+                if (slot.State.type == state.type)
+                {
+                    slot.State.amount -= state.amount;
+                    slot.State = slot.state;
+                }
             }
         }
 
         public void AddState(State state)
         {
-            if (m_StateSlots.Find(slot => slot.state.type == state.type).State != null)
+            foreach (var slot in m_StateSlots)
             {
-                State temp = m_StateSlots.Find(slot => slot.state.type == state.type).State;
-                temp.name = m_StateSlots.Find(slot => slot.state.type == state.type).State.name;
-                temp.amount = m_StateSlots.Find(slot => slot.state.type == state.type).State.amount + state.amount;
-                m_StateSlots.Find(slot => slot.state.type == state.type).State = temp;
+                if (slot.State.type == state.type)
+                {
+                    slot.State.amount += state.amount;
+                    slot.State = slot.state;
+                }
             }
         }
     }
