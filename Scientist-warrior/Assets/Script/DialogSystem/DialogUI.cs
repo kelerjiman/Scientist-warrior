084f7ed35a06a8ec7fde79b0155fdcb5c3271686
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-05 21:28:42 +0430

    Create DialogUI.cs

---
+++ Scientist-warrior/Assets/Script/DialogSystem/DialogUI.cs
@@ -0,0 +1,29 @@
+﻿using Script.DialogSystem;
+using UnityEngine;
+using TMPro;
+
+public class DialogUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI MessageHolder;
+    private Vector3 defPosition;
+    void Start()
+    {
+        defPosition = transform.position;
+        EventManager.OnSendindDialog+= OnSendindDialog;
+        EventManager.OnCloseDialog+= OnCloseDialog;
+        
+    }
+
+    private void OnCloseDialog()
+    {
+        MessageHolder.text = string.Empty;
+        gameObject.transform.position = defPosition;
+    }
+
+    private void OnSendindDialog(DialogSender dialogSender)
+    {
+        MessageHolder.text = dialogSender.Dialog.Messages[0].massage;
+        gameObject.transform.position = dialogSender.DialogPlaceHolder.position;
+        
+    }
+}
