abec7bb6f5fe4db21d4f666552e85be0f0059da2
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-05 21:28:27 +0430

    Create Dialog.cs

---
+++ Scientist-warrior/Assets/Script/DialogSystem/Dialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Script.DialogSystem
+{
+    [Serializable]
+    public class Dialog
+    {
+        public string Name;
+        public int Id;
+        public int SenderId;
+        public List<Message> Messages ;
+        public List<Message> TargetMessage;
+    }
+
+    public enum DialogType
+    {
+        None
+    }
+
+    [Serializable]
+    public struct Message
+    {
+        public string Title;
+
+        [TextArea] public string massage;
+
+    }
+}
