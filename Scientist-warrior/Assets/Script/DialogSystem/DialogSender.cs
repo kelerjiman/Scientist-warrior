b326d895aed080cac8766d92f5b7c63955747b79
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-05 21:28:37 +0430

    Create DialogSender.cs

---
+++ Scientist-warrior/Assets/Script/DialogSystem/DialogSender.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Script.DialogSystem
+{
+    public class DialogSender : MonoBehaviour
+    {
+        [SerializeField] private int Id;
+        public Dialog Dialog;
+        public Transform DialogPlaceHolder;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.CompareTag("Player"))
+                SendDialog();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                CloseDialog();
+            }
+        }
+
+        private void Start()
+        {
+            Id = Random.Range(0, 100000);
+            Dialog.SenderId = Id;
+        }
+
+        public void SendDialog()
+        {
+            EventManager.OnSendindDialog?.Invoke(this);
+        }
+
+        public void CloseDialog()
+        {
+            EventManager.OnCloseDialog?.Invoke();
+        }
+    }
+}
