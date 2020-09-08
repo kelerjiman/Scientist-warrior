a6b5ba6d72e2fcf3fa17306659efae0a2e4ca1e1
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-08 14:43:38 +0430

    Update EventManager.cs

--- Scientist-warrior/Assets/EventManager.cs
+++ Scientist-warrior/Assets/EventManager.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Script;
 using Script.DialogSystem;
+using Script.QuestSystem;
 using UnityEngine;
 
 public static class EventManager
 {
-    public static Action<ItemSlot> EquipItem;
-    public static Action<ItemSlot> UnEquipItem;
-
     public static Action<DialogSender> OnSendindDialog;
+    public static Action<DialogSender> OnCompeleteDialog;
     public static Action OnCloseDialog;
 }
