d516a4f3514192b6c9ea06bbeea906ea031e15bd
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-08 14:40:17 +0430

    Create GameManager.cs

---
+++ Scientist-warrior/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool GameContinue = true;
+    public static GameManager Instance;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
