4f9e1b2b20a65831ad5ece3d5b2df57681fc0de7
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-14 16:03:38 +0430

    Create ShopItem.cs

---
+++ Scientist-warrior/Assets/Script/ShopSystem/ShopItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.ShopSystem
+{
+    [Serializable]
+    public class ShopItem
+    {
+        public Item item;
+        [TextArea] public string summary;
+        public int Amount = 1;
+        
+    }
+}
