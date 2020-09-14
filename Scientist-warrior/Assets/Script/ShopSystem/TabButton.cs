34c1d234d3b4880553fdfa67cb6c6d7f06234f3c
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-14 16:04:06 +0430

    Create TabButton.cs

---
+++ Scientist-warrior/Assets/Script/ShopSystem/TabButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.ShopSystem
+{
+    public class TabButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        public string Name;
+        public ButtonType type;
+        public Action<ButtonType> buttonTypeOnclickEvent;
+
+        private void Start()
+        {
+            button.onClick.AddListener(OnClick);
+        }
+        private void OnClick()
+        {
+            buttonTypeOnclickEvent?.Invoke(type);
+        }
+    }
+
+    public enum ButtonType
+    {
+        Buy,
+        Sell
+    }
+}
