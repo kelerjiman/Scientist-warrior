795f4e0c260919b11d8df954e2bcffb83ab500ed
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-14 16:03:33 +0430

    Create Price.cs

---
+++ Scientist-warrior/Assets/Script/ShopSystem/Price.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Script.ShopSystem
+{
+    [Serializable]
+    public class Price
+    {
+        public PriceType type;
+        public int goldAmount = 1;
+        public int gemAmount = 1;
+    }
+
+    public enum PriceType
+    {
+        Gold,
+        Gem,
+        Both
+    }
+}
