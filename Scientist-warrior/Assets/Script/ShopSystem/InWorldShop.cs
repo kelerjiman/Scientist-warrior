07efcabbc71c1083f26c83421db187b9eb6e4e9c
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-09-14 16:03:27 +0430

    Create InWorldShop.cs

---
+++ Scientist-warrior/Assets/Script/ShopSystem/InWorldShop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.ShopSystem
+{
+    public class InWorldShop : MonoBehaviour
+    {
+        [SerializeField] private List<ShopItem> m_ShopItems;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                ShopManager.Instance.m_ShopUi.SetData(m_ShopItems, this);
+                ShopManager.Instance.m_ShopUi.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                ShopManager.Instance.m_ShopUi.gameObject.SetActive(false);
+            }
+        }
+    }
+}
