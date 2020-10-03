9c75e7953492725ea6913cfa813040183f1c6459
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-10-03 14:22:49 +0330

    Create CombatBase.cs

---
+++ Scientist-warrior/Assets/Script/CombatSystem/CombatBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Script.CombatSystem
+{
+    public class CombatBase : MonoBehaviour, IINteractable
+    {
+        [Space(10)]
+        [Header("Base Combat")]
+        [SerializeField] internal int CurrentHealth = 1;
+        [SerializeField] internal int CurrentEnergy = 1;
+        [SerializeField] internal Transform AttackPoint;
+        [SerializeField] internal float Radius = 1;
+        [SerializeField] internal LayerMask TargetLayer;
+        [SerializeField] internal float attackSpeed;
+        public virtual void GetDamage(int damage)
+        {
+            CurrentHealth -= damage;
+        }
+    }
+}
