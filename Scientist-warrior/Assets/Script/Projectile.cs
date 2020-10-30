e0f9bca69cd61a90f29abb6aef7a5afb3d583751
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-10-30 19:40:42 +0330

    Create Projectile.cs

---
+++ Scientist-warrior/Assets/Script/Projectile.cs
@@ -0,0 +1,55 @@
+﻿using Script.CombatSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float destroyDlyTime = 0.2f;
+    public Vector2 velocity = Vector2.zero;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] ParentType parentType;
+    public int damage = 1;
+    private void Start()
+    {
+        rb.velocity = velocity;
+        Destroy(gameObject, destroyDlyTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (parentType == ParentType.Player)
+        {
+            if (!collision.CompareTag("Player"))
+            {
+                var coll = collision.gameObject.GetComponent<IINteractable>();
+                if (coll != null)
+                    coll.GetDamage(damage);
+            }
+
+        }
+        else if (parentType == ParentType.Blast)
+        {
+            var coll = collision.gameObject.GetComponent<IINteractable>();
+            if (coll != null)
+                coll.GetDamage(damage);
+        }
+        else
+        {
+            if (!collision.CompareTag("Other"))
+            {
+                var coll = collision.gameObject.GetComponent<IINteractable>();
+                if (coll != null)
+                    coll.GetDamage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+
+    }
+    enum ParentType
+    {
+        Player,
+        Other,
+        Blast
+    }
+}
