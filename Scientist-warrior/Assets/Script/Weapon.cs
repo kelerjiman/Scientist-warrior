a11459ad1e65a17ddb5e63e1b03614db32578aaf
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-10-30 19:49:49 +0330

    Create Weapon.cs

---
+++ Scientist-warrior/Assets/Script/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Weapon_", menuName = "Weapon")]
+public class Weapon : EquipableItem
+{
+    private void OnValidate()
+    {
+        Properties.Type = BodyPartType.Weapon;
+        if (projectile == null)
+            EquipmentType = EquipmentType.MainHand;
+        else
+            EquipmentType = EquipmentType.Range;
+
+    }
+    public Projectile projectile;
+    public AttackType attackType;
+}
