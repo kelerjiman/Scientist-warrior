f1d1f8ab532a2c0384c68a113cdf250a8db64f69
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-10-30 19:40:14 +0330

    Update EquipmentSlot.cs

--- Scientist-warrior/Assets/Script/InventorySystem/EquipmentSlot.cs
+++ Scientist-warrior/Assets/Script/InventorySystem/EquipmentSlot.cs
@@ -7,20 +7,29 @@ using UnityEngine;
 public class EquipmentSlot : ItemSlot
 {
     public EquipmentType equipmentType;
-    
+
     public override bool CanRecieveItem(Item item)
     {
         if (item == null)
         {
             return true;
         }
-        EquipableItem equipableItem= item as EquipableItem;
-        return equipableItem != null && equipableItem.EquipmentType == equipmentType;
+        EquipableItem equipableItem = item as EquipableItem;
+        if (equipmentType == EquipmentType.MainHand)
+        {
+            if (equipableItem != null && (equipableItem.EquipmentType == EquipmentType.MainHand ||
+                equipableItem.EquipmentType == EquipmentType.Range))
+                return true;
+            else
+                return false;
+        }
+        else
+            return equipableItem != null && equipableItem.EquipmentType == equipmentType;
     }
 
     protected override void OnValidate()
     {
-        gameObject.name = equipmentType.ToString()+" Slot";
+        gameObject.name = equipmentType.ToString() + " Slot";
         base.OnValidate();
     }
 }
