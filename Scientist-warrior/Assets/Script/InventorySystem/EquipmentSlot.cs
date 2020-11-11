afdff927ed8c12edcd6e2ca811cea20a3ba9d35e
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-11-11 11:21:46 +0330

    Update EquipmentSlot.cs

--- Scientist-warrior/Assets/Script/InventorySystem/EquipmentSlot.cs
+++ Scientist-warrior/Assets/Script/InventorySystem/EquipmentSlot.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 
 public class EquipmentSlot : ItemSlot
 {
-    public EquipmentType equipmentType;
-
+    public EquipmentType EquipmentType;
     public override bool CanRecieveItem(Item item)
     {
         if (item == null)
@@ -15,21 +14,44 @@ public class EquipmentSlot : ItemSlot
             return true;
         }
         EquipableItem equipableItem = item as EquipableItem;
-        if (equipmentType == EquipmentType.MainHand)
+        //if (equipmentType == EquipmentType.MainHand)
+        //{
+        //    if (equipableItem != null && (equipableItem.EquipmentType == EquipmentType.MainHand ||
+        //        equipableItem.EquipmentType == EquipmentType.Range))
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        if (EquipmentType == EquipmentType.Shield)
         {
-            if (equipableItem != null && (equipableItem.EquipmentType == EquipmentType.MainHand ||
-                equipableItem.EquipmentType == EquipmentType.Range))
+            if (equipableItem != null && (
+                equipableItem.EquipmentType == EquipmentType.Shield ||
+                equipableItem.EquipmentType == EquipmentType.Range ||
+                equipableItem.EquipmentType == EquipmentType.offHand
+                ))
                 return true;
             else
                 return false;
         }
+        if (EquipmentType == EquipmentType.MainHand)
+        {
+            if (equipableItem != null && (
+                  equipableItem.EquipmentType == EquipmentType.MainHand ||
+                  equipableItem.EquipmentType == EquipmentType.Staff    ||
+                  equipableItem.EquipmentType == EquipmentType.TwoHand
+                  ))
+                return true;
+            else
+                return false;
+
+        }
         else
-            return equipableItem != null && equipableItem.EquipmentType == equipmentType;
+            return equipableItem != null && equipableItem.EquipmentType == EquipmentType;
     }
 
     protected override void OnValidate()
     {
-        gameObject.name = equipmentType.ToString() + " Slot";
+        gameObject.name = EquipmentType.ToString() + " Slot";
         base.OnValidate();
     }
 }
