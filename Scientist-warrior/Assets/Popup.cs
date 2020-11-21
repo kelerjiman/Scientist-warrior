d2558c85bc2a5dd6a93823ee74af8425b6d1e4fc
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-11-21 20:57:14 +0330

    Create Popup.cs

---
+++ Scientist-warrior/Assets/Popup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using Script.CurrencySystem;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(UIAnimation))]
+public class Popup : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI dataText;
+    [SerializeField] private TextMeshProUGUI TitleText;
+    [SerializeField] private float lifeTime=0.5f;
+    private UIAnimation _uiAnimation;
+
+    private void Start()
+    {
+        
+        _uiAnimation = GetComponent<UIAnimation>();
+        CurrencyManager.Instance.SendLevelEvent += CurrencyManagerOnSendLevelEvent;
+    }
+
+    void CurrencyManagerOnSendLevelEvent(string obj)
+    {
+        TitleText.text = "LEVEL UPDATED !";
+        dataText.text = obj;
+        _uiAnimation.OpenPanel();
+        StartCoroutine(LifeCycle());
+    }
+
+    IEnumerator LifeCycle()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        _uiAnimation.ClosePanel();
+    }
+}
